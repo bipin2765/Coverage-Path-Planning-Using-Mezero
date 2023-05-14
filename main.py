379501a2dad8735ce4c7c220cfddc84fdf05fe23
Subash Ale Magar <73476442+bipin2765@users.noreply.github.com>
2023-05-14 14:41:40 +0200

    commiting muzero code on 10/05/2023

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import argparse
 import os
-import json
-
-from types import SimpleNamespace as Namespace
 
 from src.CPP.Environment import CPPEnvironmentParams, CPPEnvironment
 
@@ -16,9 +13,40 @@ def main_cpp(p):
 
 
 if __name__ == "__main__":
-    config_json_loc = "config/manhattan32_cpp.json"
-    with open(config_json_loc, "r") as config_file:
-        params = json.loads(config_file.read(), object_hook=lambda d: Namespace(**d))
 
-        # Run Model
-        main_cpp(p=params)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gpu', action='store_true', help='Activates usage of GPU')
+    parser.add_argument('--generate_config', action='store_true', help='Enable to write default config only')
+    parser.add_argument('--config', default=None, help='Path to config file')
+    parser.add_argument('--id', default=None, help='If set overrides the logfile name and the save name')
+
+    parser.add_argument('--params', nargs='*', default=None)
+
+    parser.add_argument('--cpp', action='store_true', help='Run Coverage Path Planning')
+    args = parser.parse_args()
+
+    if args.generate_config:
+        if args.cpp:
+            generate_config(CPPEnvironmentParams(), "config/cpp.json")
+
+        exit(0)
+
+    if args.config is None:
+        print("Config file needed!")
+        exit(1)
+
+    if not args.gpu:
+        os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+    params = read_config(args.config)
+
+    if args.params is not None:
+        params = override_params(params, args.params)
+
+    if args.id is not None:
+        params.model_stats_params.save_model = "models/" + args.id
+        params.model_stats_params.log_file_name = args.id
+
+    if args.cpp:
+        main_cpp(params)
+
